6dec939d21f06c40343c0a0dfa508e9be9b7864a
onefollower <wangkai_like@163.com>
2023-08-19 03:59:01 +0000

    finish all cpu test

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,15 +6,67 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+    char * s;
+    int d;
+    int ret = 0;
+    while(*fmt)
+    {
+        if ((*fmt) != '%')
+        {
+            out[ret++] = *fmt;
+            fmt++;
+            continue;
+        }
+        fmt++;
+        switch(*fmt++){
+            case 's':
+                {
+                    s = va_arg(ap, char*);
+                    size_t s_len = strlen(s);
+                    size_t i = 0;
+                    while (s_len--)
+                    {
+                        out[ret++] = s[i++];
+                    }
+                    continue;
+                }
+            case 'd':
+                {
+                    d = va_arg(ap, int);
+                    int l = ret;
+                    int r = l-1; 
+                    while (d)
+                    {
+                        out[ret++] = d % 10 + '0';
+                        d = d / 10;
+                        r++;
+                    }
+                    while (l < r)
+                    {
+                       char t = out[l];
+                       out[l] = out[r];
+                       out[r] = t;
+                       l++;
+                       r--;
+                    }
+                    continue;
+                }
+        }
+    }
+    out[ret++] = '\0';
+    return ret;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+    va_list ap;
+    va_start(ap, fmt);
+    int ret = vsprintf(out, fmt, ap);
+    va_end(ap);
+    return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
